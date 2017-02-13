7ecebaf5564db8c8b45842b8a6a9f4a02d6b4b9f
Alexander <ac3433@drexel.edu>
2017-02-13 11:57:59 -0500

    adding minor fog and first island trigger

---
+++ Project2/Assets/Scripts/Environments/IslandTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IslandTrigger : MonoBehaviour {
+
+    public GameObject[] Walls;
+
+    public SinkFloat island;
+
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject wall in Walls)
+        {
+            Collider collider = wall.GetComponent<Collider>();
+            collider.enabled = false;
+        }
+
+        island.enabled = true;
+    }
+}
---
+++ Project2/Assets/Scripts/Environments/SinkFloat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinkFloat : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 2f;
+
+    Transform initalPos;
+
+    void Start () {
+        initalPos = transform;
+	}
+
+
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        if (target.position == transform.position)
+        {
+            enabled = false;
+        }
+
+    }
+}
